7e4d2c0438742a16f6329d5714e7274c2cd4e788
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-05-26 08:07:50 +0100

    bindings: SPC TAB to toggle last buffer

--- mappings.lua
+++ mappings.lua
@@ -8,6 +8,7 @@ return {
   n = {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
+    ["<leader><tab>"] = { "<cmd>b#<cr>", desc = "Last tab" },
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
