259ba94af2d53df0c664349ee9b2a9a9d956fcdf
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-05-26 08:06:12 +0100

    options: set localleader
    define localleader key binding for Clojure and other packages that use
    mode specific menus

--- options.lua
+++ options.lua
@@ -15,6 +15,7 @@ return {
     autopairs_enabled = true, -- enable autopairs at start
     diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+    maplocalleader = ",",            -- Set local leader key binding (supports Conjure key bindings)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
   },
 }
