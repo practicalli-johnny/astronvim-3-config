1b01c9547ddcd1cb3fd672df790ced9966163083
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-05-27 20:26:49 +0100

    mappings: format updates from lua language server

--- mappings.lua
+++ mappings.lua
@@ -12,7 +12,9 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+          require("astronvim.utils.buffer").close(bufnr)
+        end)
       end,
       desc = "Pick to close",
     },
