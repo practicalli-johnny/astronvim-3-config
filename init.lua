b0b8a4559a8b22fffdadd184580225a9d2e34e2f
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-05-30 11:44:27 +0100

    init: remove duplicate key

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@ return {
   -- Configure AstroNvim updates
   updater = {
     remote = "origin",     -- remote to use
-    remote = "origin",     -- remote to use
     channel = "stable",    -- "stable" or "nightly"
     version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
     branch = "nightly",    -- branch name (NIGHTLY ONLY)
