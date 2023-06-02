d7cce54168e7f6d201e9aa20b68544e7b0eeec21
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-06-02 01:17:02 +0100

    treesitter: additional language parsers
    - "fennel",
    - "git_config",
    - "gitignore",
    - "graphql",
    - "http",
    - "jq",
    - "make",
    - "markdown",  -- markdown pack includes agressive prettierd
    - "markdown_inline",
    - "org",
    - "python",
    - "r",
    - "regex",
    - "sql",
    - "yaml", -- yaml pack includes agressive prettierd

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,6 +3,22 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+      -- `community.lua` ensures parsers via their language packs
+      "fennel",
+      "git_config",
+      "gitignore",
+      "graphql",
+      "http",
+      "jq",
+      "make",
+      "markdown",
+      "markdown_inline",
+      "org",
+      "python",
+      "r",
+      "regex",
+      "sql",
+      "yaml", -- yaml pack includes agressive prettierd
     })
   end,
 }
