baf3ae9e6469b8bccb2fa478995de377ce57c4d1
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-06-02 01:38:37 +0100

    options: clean up configuration file

--- options.lua
+++ options.lua
@@ -7,19 +7,19 @@ return {
     spell = false,         -- sets vim.opt.spell
     signcolumn = "auto",   -- sets vim.opt.signcolumn to auto
     wrap = true,           -- sets vim.opt.wrap
-    showtabline = 0        -- sets vim.opt.showtabline
+    -- showtabline = 0,    -- sets vim.opt.showtabline - zero hides tabs
     timeoutlen = 420,
   },
   g = {
     mapleader = " ",                 -- sets vim.g.mapleader
-    maplocalleader = ",",            -- Set local leader key binding (supports Conjure key bindings)
+    -- maplocalleader = ",",         -- Set local leader key binding (defined in AstroNvim)
     autoformat_enabled = true,       -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
     cmp_enabled = true,              -- enable completion at start
     autopairs_enabled = true,        -- enable autopairs at start
     diagnostics_mode = 3,            -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     icons_enabled = true,            -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
-    VM_leader = "gm"                 -- Visual Multi Leader (multiple cursors)
+    VM_leader = "gm",                -- Visual Multi Leader (multiple cursors)
   },
 }
 -- If you need more control, you can use the function()...end notation
