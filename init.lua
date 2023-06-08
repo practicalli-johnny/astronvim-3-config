5216df8c20487b445c8e37a10b0cebfb4f045b94
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-06-08 12:50:53 +0100

    lsp: disable markdown and prettier format on save
    Markdown formatting too agressive with Material for MkDoc features

--- init.lua
+++ init.lua
@@ -37,11 +37,14 @@ return {
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           "lua",
+          "markdown",
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
+        "prettier",
+        "prettierd",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
