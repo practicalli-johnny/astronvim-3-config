844e218a7dfcdaa6f9b72698d33b7e3e7470a935
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-07-02 15:50:28 +0100

    community: format config

--- plugins/community.lua
+++ plugins/community.lua
@@ -2,6 +2,7 @@
 -- https://github.com/AstroNvim/astrocommunity
 
 return {
+  -- ----------------------------------------------
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
 
@@ -11,18 +12,24 @@ return {
   --   "AstroNvim/astrocommunity",
   --   dev = true,
   -- },
+  -- local plugins in path
+  -- { dir = "~/projects/community/neovim/astrocommunity" },
+  -- ----------------------------------------------
 
+  -- ----------------------------------------------
   -- Motion plugins
   -- :help nvim-surround.usage.
   { import = "astrocommunity.motion.nvim-surround" },
+  -- ----------------------------------------------
 
-
+  -- ----------------------------------------------
   -- Editor support
   -- Rainbow parens
   { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
   { import = "astrocommunity.editing-support.nvim-regexplainer" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
 
+  -- ----------------------------------------------
   -- Distraction free editing
   { import = "astrocommunity.editing-support.zen-mode-nvim" },
   {
@@ -41,7 +48,9 @@ return {
       },
     },
   },
+  -- ----------------------------------------------
 
+  -- ----------------------------------------------
   -- Packs
   -- Treesitter: clojure , Lsp: clojure-lsp, Lint/format:
   { import = "astrocommunity.pack.clojure" },
@@ -57,16 +66,25 @@ return {
   -- { import = "astrocommunity.pack.markdown" },
   -- Treesitter: markdown & markdown_inline, Lsp: marksman, Lint/format: prettierd
   -- { import = "astrocommunity.pack.yaml" },
+  -- ----------------------------------------------
 
+  -- ----------------------------------------------
   -- Project
   { import = "astrocommunity.project.project-nvim" }, -- switch between projects
+  -- ----------------------------------------------
 
+  -- ----------------------------------------------
   -- Themes
   -- defined in themes.lua
+  -- ----------------------------------------------
 
+  -- ----------------------------------------------
   -- Utility
   { import = "astrocommunity.utility.noice-nvim" }, -- switch between projects
 
   -- Version Control
   -- defined in plugins/git.lua
+
+  -- { "max397574/better-escape.nvim", enabled = false },
+  -- ----------------------------------------------
 }
