a3675cfe1bbd1b54fa6c305527a87d39ca485651
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-08-28 15:46:18 +0100

    treesitter: bash parser to support noice plugin

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       -- `community.lua` ensures parsers via their language packs
+      "bash", -- noice
       "fennel",
       "git_config",
       "gitignore",
