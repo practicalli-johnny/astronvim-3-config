c681fe69f3fc0edb8bef0236a5a08e4a2aa99e01
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-11-13 18:39:40 +0000

    core: show hidden files in neotree by default
    Show hidden files and directorys such at `.github` directory by default in neotree, without having to show them with `H`
    
    Hidden files & directories are visually different to others
    
    `H` still toggles the visibility of hidden files and directories

--- plugins/core.lua
+++ plugins/core.lua
@@ -95,4 +95,21 @@ return {
       -- background_color = "#000000",
     },
   },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          -- when true, they will just be displayed differently than normal items
+          visible = true,
+          -- remains hidden even if visible is toggled to true, this overrides always_show
+          never_show = {
+            ".DS_Store",
+            "thumbs.db",
+          },
+        },
+      },
+    },
+  },
 }
